001df7a6e0b8b80726208d65e95e0422c688b0a5
DanielWikarski <=>
2026-05-19 18:23:51 +0200

    Zrobiono pierwszą część zadań

---
+++ solved_zadania_14052026.py
@@ -0,0 +1,87 @@
+# Zadanie 1
+
+print("=== Zadanie 1 ===")
+user_age = int(input("Podaj swój wiek: "))
+
+if user_age < 12:
+    print("Możesz obejrzeć tylko bajkę")
+elif 12 <= user_age <= 17:
+    print("Czy masz zgodę rodzica? (T/N <- Wpisz T - TAK / N - NIE)")
+    parent_signature = str(input().upper())
+    if parent_signature == "T":
+        print("Możesz wejśc na film")
+    else:
+        print("Możesz obejrzeć tylko bajkę")
+else:
+    print("Możesz obejrzeć dowolny film")
+
+# Zadanie 2
+
+print("=== Zadanie 2 ===")
+
+
+books_read = int(input("Ile książek przeczytał_ś w tym miesiącu? "))
+
+if not books_read:
+    print("Musisz zacząć czytać!")
+elif 1 <= books_read <= 3:
+    print("Dobry początek")
+else:
+    print("Super! Jesteś molem książkowym")
+
+
+# Zadanie 3
+
+print("=== Zadanie 3 ===")
+
+
+current_temperature = int(input("Podaj aktualną temperature (w stopniach celsjusza): "))
+
+if  current_temperature < 0:
+    print("Jest mróz, ubierz czapkę!")
+elif 0 <= current_temperature <= 15:
+    print("Jest chłodno, załóż kurtkę.")
+else:
+    print("Ciepło, możesz iśc w koszulce")
+
+# Zadanie 4
+
+print("=== Zadanie 4 ===")
+
+users_pet = str(input("Jakie masz zwierze? (pies / kot / rybka): "))
+
+if users_pet == "pies":
+    users_pet_dog_question = str(input("Czy lubisz chodzić na spacery? (T/N <- Wpisz T - TAK / N - NIE) ").upper())
+    if users_pet_dog_question == "T":
+        print("Twój pies będzie szczęśliwy!")
+    else:
+        print("Pies potrzebuje ruchu!")
+elif users_pet == "kot":
+    print("Kot lubi drapanie i spanie")
+elif users_pet == "rybka":
+    print("Pamiętaj o czystej wodzie!")
+else:
+    print("Nie znam takiego zwierzaka...")
+
+
+# Zadanie 5
+
+print("=== Zadanie 5 ===")
+
+product_price = float(input("Podaj cenę produktu: "))
+
+product_price_feedback = "Drogi" if product_price > 100 else "Tani"
+print(product_price_feedback)
+
+
+
+
+
+
+
+
+
+
+
+
+
